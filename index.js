a721b686674bba5c11e2f8049c2004d5fdc55dfe
nicramu <110261550+nicramu@users.noreply.github.com>
2022-08-05 23:05:05 +0200

    a

--- index.js
+++ index.js
@@ -9,6 +9,22 @@ function getAccel(){
   });
 }
 
+function ClickRequestDeviceMotionEvent () {
+  window.DeviceMotionEvent.requestPermission()
+    .then(response => {
+      if (response === 'granted') {
+        window.addEventListener('devicemotion',
+          () => { console.log('DeviceMotion permissions granted.') },
+          (e) => { throw e }
+      )} else {
+        console.log('DeviceMotion permissions not granted.')
+      }
+    })
+    .catch(e => {
+      console.error(e)
+    })
+}
+
 var posX = document.getElementById("pos-x"),
     posY = document.getElementById("pos-y"),
     posZ = document.getElementById("pos-z"),
