97e684382498744732e308c4d9e60d7d16173736
nicramu <110261550+nicramu@users.noreply.github.com>
2022-08-05 20:38:51 +0200

    a

--- index.js
+++ index.js
@@ -2,16 +2,31 @@
 
 window.onload = function () {
 
-getAccel()
-}
-
-function getAccel(){
-  DeviceMotionEvent.requestPermission().then(response => {
-      if (response == 'granted') {
-          console.log("accelerometer permission granted");
-          // Do stuff here
+  let accelerometer = null;
+  try {
+    accelerometer = new Accelerometer({ frequency: 10 });
+    accelerometer.onerror = (event) => {
+      // Handle runtime errors.
+      if (event.error.name === 'NotAllowedError') {
+        console.log('Permission to access sensor was denied.');
+      } else if (event.error.name === 'NotReadableError') {
+        console.log('Cannot connect to the sensor.');
       }
-  });
+    };
+    accelerometer.onreading = (e) => {
+      console.log(e);
+    };
+    accelerometer.start();
+  } catch (error) {
+    // Handle construction errors.
+    if (error.name === 'SecurityError') {
+      console.log('Sensor construction was blocked by the Permissions Policy.');
+    } else if (error.name === 'ReferenceError') {
+      console.log('Sensor is not supported by the User Agent.');
+    } else {
+      throw error;
+    }
+  }
 }
 
 function ClickRequestDeviceMotionEvent () {
