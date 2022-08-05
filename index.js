8986e4b376341608346ad30e2cbf07732f0f105d
nicramu <110261550+nicramu@users.noreply.github.com>
2022-08-05 20:17:31 +0200

    a

--- index.js
+++ index.js
@@ -2,17 +2,17 @@
 
 window.onload = function () {
 
-  // Check if is IOS 13 when page loads.
-  if ( window.DeviceMotionEvent && typeof window.DeviceMotionEvent.requestPermission === 'function' ){
-
-      // Everything here is just a lazy banner. You can do the banner your way.
-      const banner = document.createElement('div')
-      banner.innerHTML = `<div style="z-index: 1; position: absolute; width: 100%; background-color:#000; color: #fff"><p style="padding: 10px">Click here to enable DeviceMotion</p></div>`
-      banner.onclick = ClickRequestDeviceMotionEvent // You NEED to bind the function into a onClick event. An artificial 'onClick' will NOT work.
-      document.querySelector('body').appendChild(banner)
-  }
+getAccel()
 }
 
+function getAccel(){
+  DeviceMotionEvent.requestPermission().then(response => {
+      if (response == 'granted') {
+          console.log("accelerometer permission granted");
+          // Do stuff here
+      }
+  });
+}
 
 function ClickRequestDeviceMotionEvent () {
   window.DeviceMotionEvent.requestPermission()
