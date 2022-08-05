e4b6f2230086643ba56dbcc9e2dda93f2bfaa911
nicramu <110261550+nicramu@users.noreply.github.com>
2022-08-05 20:12:12 +0200

    a

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
 /*Downloaded from https://www.codeseek.co/Jeremboo/css-3d-card-moved-by-mobile-accelerometer-OyBaLa */
 
-function getAccel(){
-  DeviceMotionEvent.requestPermission().then(response => {
-      if (response == 'granted') {
-          console.log("accelerometer permission granted");
-          // Do stuff here
-      }
-  });
+window.onload = function () {
+
+  // Check if is IOS 13 when page loads.
+  if ( window.DeviceMotionEvent && typeof window.DeviceMotionEvent.requestPermission === 'function' ){
+
+      // Everything here is just a lazy banner. You can do the banner your way.
+      const banner = document.createElement('div')
+      banner.innerHTML = `<div style="z-index: 1; position: absolute; width: 100%; background-color:#000; color: #fff"><p style="padding: 10px">Click here to enable DeviceMotion</p></div>`
+      banner.onclick = ClickRequestDeviceMotionEvent // You NEED to bind the function into a onClick event. An artificial 'onClick' will NOT work.
+      document.querySelector('body').appendChild(banner)
+  }
 }
-getAccel();
+
+
+function ClickRequestDeviceMotionEvent () {
+  window.DeviceMotionEvent.requestPermission()
+    .then(response => {
+      if (response === 'granted') {
+        window.addEventListener('devicemotion',
+          () => { console.log('DeviceMotion permissions granted.') },
+          (e) => { throw e }
+      )} else {
+        console.log('DeviceMotion permissions not granted.')
+      }
+    })
+    .catch(e => {
+      console.error(e)
+    })
+}
 
 var posX = document.getElementById("pos-x"),
     posY = document.getElementById("pos-y"),
